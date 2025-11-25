08f052668188e5ee447162bf907f3dfc8458e76b
albertgabrielabdon <157718121+albertgabrielabdon@users.noreply.github.com>
2025-11-26 07:17:47 +0800

    added create and delete

--- insert.py
+++ insert.py
@@ -5,6 +5,21 @@ from datetime import datetime
 ### change to appropriate value
 HOST = "mongodb://localhost:27018"
 
+def create_posting(db, posting: dict):
+    return db["postings"].insert_one(posting)
+
+def create_history(db, entry: dict):
+    entry = {**entry, "timestamp": datetime.fromisoformat(entry["timestamp"])}
+    return db["postingHistory"].insert_one(entry)
+
+def delete_posting(db, item: dict):
+    return db["postings"].delete_one(item)
+
+def delete_many_postings(db, items: dict):
+    return db["postings"].delete_many(items)
+
+def delete_history_entry(db, user_entry: dict):
+    return db["postingHistory"].delete_one(user_entry)
 
 if __name__ == "__main__":
     # insert into database
